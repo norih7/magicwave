fafd2ed2569b74788014ecb7d26c65f2f089b28b
norih <>
2026-06-20 18:15:29 +0900

    トップページのmeta情報を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-// src/app/page.tsx
-import Link from "next/link";
-import { db } from "@/db";
-import { items } from "@/db/schema";
 import SetPageTitle from "@/components/SetPageTitle";
 
 // 💡 念のため、このページは完全に静的（SSG）であることを明示します
 export const dynamic = "force-static";
 
+const title = "RE:MAGIC テイルズオブエターニア攻略";
+export const metadata = {
+  title,
+  description: "テイルズオブエターニア攻略サイト。PS版、PSP版、リマスター版に対応しています。",
+};
+
 export default async function HomePage() {
-  // 💡 ビルド時に、JSON (Drizzle) から登録されている全アイテムを安全に一括取得！
-  const allItems = await db.select().from(items).all();
-  const title = "テイルズオブエターニア攻略";
+  const subTitle = "テイルズオブエターニア攻略";
   return (
     <div style={{ padding: "0px" }}>
-      <SetPageTitle title={title} />
+      <SetPageTitle title={subTitle} />
       <h1>テイルズオブエターニア攻略サイト</h1>
       <p>2026/06/20ページ公開 レイアウト崩れなどこれから直していきます</p>
     </div>
